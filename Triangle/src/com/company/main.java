b1ff890d5ac7cb5de055ff17517e0d8b4551207a
Tran Trung Kien <tieulong2803200@gmail.com>
2021-04-09 15:00:30 +0700

    Bài tập lớp Shape-Triangle

---
+++ Triangle/src/com/company/main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class main {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        Triangle triangle = new Triangle(1,2,3);
+        System.out.println(triangle.getArea());
+    }
+}
