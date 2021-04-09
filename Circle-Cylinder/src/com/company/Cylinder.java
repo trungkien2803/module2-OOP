c34f347f33e646e760182faf9de7ad1cf5c85ace
Tran Trung Kien <tieulong2803200@gmail.com>
2021-04-09 13:38:56 +0700

    Bài tập kế thừa Circle-Cylinder

---
+++ Circle-Cylinder/src/com/company/Cylinder.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Cylinder extends Circle {
+    public double height;
+
+    public Cylinder() {
+
+    }
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius,color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return getArea() * this.height;
+    }
+}
---
+++ Circle-Cylinder/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(3,"red",10);
+        System.out.println(cylinder.getVolume());
+
+    }
+}
