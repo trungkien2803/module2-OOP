8ecc6304f018be986400fafa98886a054f22972a
Tran Trung Kien <tieulong2803200@gmail.com>
2021-04-12 08:42:54 +0700

    Thực hành Lớp Animal và interface Edible

---
+++ AnimalInterfaceEdible/src/com/company/Chicken.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Chicken extends Animal implements Edible {
+    @Override
+    public String makeSound() {
+        return "Chicken: cluck-cluck!";
+    }
+
+    @Override
+    public String howToEat() {
+        return "could be fried";
+    }
+}
---
+++ AnimalInterfaceEdible/src/com/company/Main.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public abstract class Main {
+    public abstract String makeSound();
+}
