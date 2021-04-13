4c90afcd8f3f147dbf88ac45b29630e071527fa9
Tran Trung Kien <tieulong2803200@gmail.com>
2021-04-13 17:02:48 +0700

    [Bài tập] Tính ngày tiếp theo

--- NextDayCalculator/test/com/company/NextDayCalculatorTest.java
+++ NextDayCalculator/test/com/company/NextDayCalculatorTest.java
@@ -6,17 +6,26 @@ class NextDayCalculatorTest {
 
     @org.junit.jupiter.api.Test
     void checkNumberDayOfMonth() {
+        int month = 1;
+        int year = 2018;
+        int expected = 31;
+        int result = NextDayCalculator.checkNumberDayOfMonth(month, year);
+        assertEquals(expected, result);
     }
 
     @org.junit.jupiter.api.Test
     void checkLeapYear() {
+        int year = 2020;
+        boolean expected = true;
+        boolean result = NextDayCalculator.checkLeapYear(year);
+        assertEquals(expected, result);
     }
 
     @org.junit.jupiter.api.Test
     void calculateNextDay() {
-    }
+        int day = 28;
+        int month = 2;
+        int year = 2019;
 
-    @org.junit.jupiter.api.Test
-    void main() {
     }
 }
