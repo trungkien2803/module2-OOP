e531b61528b5b7f6e1ac0d1c3552b6a1453b8cf1
Tran Trung Kien <tieulong2803200@gmail.com>
2021-04-28 09:23:32 +0700

    [Thực hành] Racing Cars

---
+++ RacingCars/src/com/company/RacingCars.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Random;
+
+class Car implements Runnable {
+
+    private String name;
+    public static int DISTANCE = 100;
+    public static int STEP = 1;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        int runDistance = 0;
+        long startTime = System.currentTimeMillis();
+
+        while (runDistance < DISTANCE) {
+            try {
+                int speed = (new Random()).nextInt(20);
+                runDistance += speed;
+                String log = "|";
+                int percentTravel = (runDistance * 100) / DISTANCE;
+                for (int i = 0; i < DISTANCE; i += STEP) {
+                    if (percentTravel >= i + STEP) {
+                        log += "=";
+                    } else if (percentTravel >= i && percentTravel < i + STEP) {
+                        log += "o";
+                    } else {
+                        log += "-";
+                    }
+                }
+                log += "|";
+                System.out.println("Car" + this.name + ": " + log + " " + Math.min(DISTANCE, runDistance) + "KM");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Car" + this.name + " broken...");
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Car" + this.name + " Finish in " + (endTime - startTime) / 1000 + "s");
+    }
+}
+
