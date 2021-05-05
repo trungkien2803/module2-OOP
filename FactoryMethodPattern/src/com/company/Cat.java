f420bb1750be16fa002b3e02521695f18928ddf1
Tran Trung Kien <tieulong2803200@gmail.com>
2021-05-05 13:51:43 +0700

    [Thực hành] Factory Method

---
+++ FactoryMethodPattern/src/com/company/Cat.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Cat extends Animal{
+    @Override
+    public String makeSound() {
+        return "Meow";
+    }
+}
---
+++ FactoryMethodPattern/src/com/company/Dog.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Dog extends Animal{
+    @Override
+    public String makeSound() {
+        return "Woof";
+    }
+}
+
