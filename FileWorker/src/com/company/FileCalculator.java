c9548932c864f57aa10e2ca330827ea0ddc1cf32
Tran Trung Kien <tieulong2803200@gmail.com>
2021-05-06 13:50:18 +0700

    [Thực hành] Triển khai Adapter

---
+++ FileWorker/src/com/company/FileCalculator.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public interface FileCalculator {
+    long calculateSize(String path);
+}
---
+++ FileWorker/src/com/company/FileCalculatorAdapter.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import com.codegym.FileUtil;
+
+import java.io.File;
+
+public class FileCalculatorAdapter implements FileCalculator{
+    @Override
+    public long calculateSize(String path) {
+        FileUtil fileUtil = new FileUtil();
+        File file = new File(path);
+        return fileUtil.calculateSize(file);
+    }
+}
